2a4868e7310cfdf6522211a89e0e25db53a55a73
kobirul5 <kobirul05j@gmail.com>
2025-02-09 11:39:52 +0600

    added education page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,14 +1,39 @@
 "use client"
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { motion } from "motion/react"
 export default function About() {
+
+
+
+  const educations = [
+    {
+      degree: "Diploma in Pharmacy",
+      institution: "SMMA IHT",
+      location: "Kajipur, Sirajganj, Bangladesh",
+      duration: "2019 - 2023"
+    },
+    {
+      degree: "HSC",
+      institution: "Sirajganj Sadar Technical and Business management institute",
+      location: "Sirajganj, Bangladesh",
+      duration: "2019 - 2021",
+      grade: "4.58/5.00",
+    },
+    {
+      degree: "SSC",
+      institution: "Chor-Khokshabari High School",
+      location: "Sirajganj, Bangladesh",
+      duration: '2016 - 2028',
+      grade: "4.58/5.00",
+    },
+  ]
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
-        transition: { delay: 2.4, duration: 2.4, ease: "easeIn" }
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
       }}
       className="min-h-[80vh] flex items-center py-12 lg:py-0"
     >
@@ -17,15 +42,40 @@ export default function About() {
           defaultValue="about-me"
           className="flex flex-col lg:flex-row mx-auto gap-44 lg:gap-6 lg:mx-0">
           <TabsList
-            className="flex flex-col  items-center w-full max-w-[380px] gap-4 "
+            className="flex flex-col  items-center w-full lg:max-w-[380px] gap-4 "
           >
             <TabsTrigger value="about-me">About Me</TabsTrigger>
-            <TabsTrigger value="Skills">Skills</TabsTrigger>
-            <TabsTrigger value="Education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
           </TabsList>
-          <div className="min-h-[70vh] w-full">
+          <div className="min-h-[70vh] w-full lg:px-10">
+            {/* about */}
             <TabsContent value="about-me" className="w-full">About Me</TabsContent>
-            
+            {/* Skills */}
+            <TabsContent value="skills" className="w-full">Skills</TabsContent>
+            {/* education */}
+            <TabsContent value="education" className="w-full">
+              <div>
+                <h1 className="text-4xl">My Education</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est sapiente praesentium eveniet assumenda iusto libero doloremque! Commodi nulla ad magni.</p>
+                <div className="grid grid-cols-1 mt-8 md:grid-cols-2 gap-6">
+                  {
+                    educations.map((education,idx)=><div key={idx}
+                    className="bg-[#27272c] p-5 rounded-sm flex flex-col gap-4" 
+                    >
+                      <h3 className="text-primaryColor">{education?.duration}</h3>
+                      <h2 className="text-2xl font-semibold">{education?.degree}</h2>
+                      <div>
+                      <h3>{education?.institution}</h3>
+                      <p>{education?.location}</p>
+                      </div>
+                    </div>)
+                  }
+                </div>
+
+              </div>
+            </TabsContent>
+
           </div>
         </Tabs>
       </div>
