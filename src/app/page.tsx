26eeb783fec8a28847e5102a558992aacc588f2b
Md. Kobirul Islam <kobirul05j@gmail.com>
2026-07-29 22:17:19 +0600

    feat: implement home page layout with portfolio sections and SEO metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export const metadata = createMetadata({
 
 export default function Home() {
   return (
-    <main className="h-full relative space-y-16">
+    <main className="h-full relative space-y-16 overflow-x-hidden">
       <section className="z-20 bg-background pt-10 px-5 ">
         <Banner></Banner>
       </section>
@@ -32,7 +32,7 @@ export default function Home() {
           <GithubCalendarSection />
         </section>
         <section className="relative">
-          <div className="absolute top-0 right-0 w-100 h-100 z-[-1] bg-primaryColor/50 rounded-[50px] blur-[100px] opacity-40 animate-pulse"></div>
+          <div className="absolute top-0 right-0 w-48 h-48 md:w-96 md:h-96 z-[-1] bg-primaryColor/50 rounded-full blur-[70px] md:blur-[100px] opacity-40 animate-pulse"></div>
           {/* projects */}
           <Heading
             subTitle="My Project"
@@ -46,7 +46,7 @@ export default function Home() {
         </section>
         {/* Backend API Projects */}
         <section className="relative">
-          <div className="absolute top-1/2 left-0 w-80 h-80 z-[-1] bg-primaryColor/30 rounded-full blur-[100px] opacity-30 animate-pulse"></div>
+          <div className="absolute top-1/2 left-0 w-48 h-48 md:w-80 md:h-80 z-[-1] bg-primaryColor/30 rounded-full blur-[70px] md:blur-[100px] opacity-30 animate-pulse"></div>
           <Heading
             subTitle="Server Side Systems"
             title1="Backend"
